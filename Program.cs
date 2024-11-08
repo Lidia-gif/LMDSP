fc74dde064b623e106add3bf7a9724b7e797b6c4
Lidia-gif <lidiamaria.202069@gmail.com>
2024-11-08 13:09:31 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+    
+public class Numero4{
+    public static void Main(string[] args)
+    {
+
+        int n1 = 7;
+        int n2 = 7;
+        int n3 = 5;
+        int n4 = 1;
+
+        if (n1 == n2)
+        {
+            Console.WriteLine("o primeiro numero é igual ao segundo");
+        }
+        else
+        {
+            Console.WriteLine("O numero não são iguais") ;
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
